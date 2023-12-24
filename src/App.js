b86675ac139b68158763e4abb317931b7798d4bc
Ashan Perera <ashantiwankaperera@gmail.com>
2023-12-24 21:03:48 +0530

    optimizing and error handling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,25 @@ import LoginButton from "./components/LoginButton";
 import LogoutButton from "./components/LogoutButton";
 import Profile from "./components/Profile";
 
+import { useAuth0 } from "@auth0/auth0-react";
+
 function App() {
+  const { isLoading, error } = useAuth0();
+
   return (
     <div className="App">
       <h1>Auth0 Authentication</h1>
-      <LoginButton />
-      <LogoutButton />
-      <Profile />
+      {error ? (
+        <p>Authentication Error</p>
+      ) : isLoading ? (
+        <p>Loading...</p>
+      ) : (
+        <>
+          <LoginButton />
+          <LogoutButton />
+          <Profile />
+        </>
+      )}
     </div>
   );
 }
